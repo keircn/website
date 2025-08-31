99d45bb1f57f6e954797b6030d76089302fe2a7e
Keiran <keircn@proton.me>
2025-08-31 17:47:59 +0100

    feat(layout): Refactor sidebar into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import Link from "next/link";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div className="font-mono min-h-screen flex text-foreground bg-background">
-      <div className="w-64 border-r border-border">
-        <div className="flex items-center mb-8 border-b border-border pb-1.5 p-2">
-          <Link href="/" className="text-3xl font-medium">
-            tuxbkt_
-          </Link>
+      <Sidebar />
+      <main className="flex-1 p-2">
+        <div className="text-sm text-muted-foreground">
+          <h1 className="text-2xl font-bold mb-4">Welcome to tuxbkt_</h1>
         </div>
-      </div>
-      <div className="flex-1 p-2">
-        <div className="text-sm text-muted-foreground"></div>
-      </div>
+      </main>
     </div>
   );
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Sidebar() {
+  return (
+    <div className="w-64 border-r border-border">
+      <div className="flex items-center mb-8 border-b border-border pb-1.5 p-2">
+        <Link href="/" className="text-3xl font-medium">
+          tuxbkt_
+        </Link>
+      </div>
+    </div>
+  );
+}
