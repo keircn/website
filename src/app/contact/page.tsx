67e4bb55e3f8e90405f2710de2685f15967e3126
Keiran <keircn@proton.me>
2025-09-04 04:56:19 +0100

    style: formatting

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,9 +3,7 @@ export default function Contact() {
     <div className="font-mono min-h-screen flex text-foreground bg-background">
       <main className="flex-1 p-2">
         <h1 className="text-2xl font-medium mb-4">Contact</h1>
-        <p className="text-sm text-muted-foreground">
-          &lt;empty&gt;
-        </p>
+        <p className="text-sm text-muted-foreground">&lt;empty&gt;</p>
       </main>
     </div>
   );
