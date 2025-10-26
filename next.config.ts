bdaafa22d9d1733196899127a0c9048216e2a72c
Keiran <keircn@proton.me>
2025-10-26 12:26:25 +0000

    caching is weird

--- next.config.ts
+++ next.config.ts
@@ -59,15 +59,6 @@ const nextConfig: NextConfig = {
           },
         ],
       },
-      {
-        source: "/(.*)\\.(jpg|jpeg|png|gif|webp|avif|ico|svg)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
     ];
   },
 };
