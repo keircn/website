d8f410697fee69357e326e3cb72e336489400052
Keiran <keircn@proton.me>
2025-12-03 02:56:31 +0000

    fix: linter

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -78,6 +78,7 @@ export default function Navbar() {
           strokeWidth="2"
           viewBox="0 0 24 24"
           stroke="currentColor"
+          aria-hidden="true"
         >
           <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
         </svg>
@@ -92,6 +93,7 @@ export default function Navbar() {
           strokeWidth="2"
           viewBox="0 0 24 24"
           stroke="currentColor"
+          aria-hidden="true"
         >
           <path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
@@ -106,6 +108,7 @@ export default function Navbar() {
           strokeWidth="2"
           viewBox="0 0 24 24"
           stroke="currentColor"
+          aria-hidden="true"
         >
           <path d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
@@ -207,6 +210,7 @@ export default function Navbar() {
                 strokeWidth="2"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
+                aria-hidden="true"
               >
                 {mobileMenuOpen ? (
                   <path d="M6 18L18 6M6 6l12 12" />
