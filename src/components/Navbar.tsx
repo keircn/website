13b96d80d0ec6b785569ff136c5fd639bee58270
Keiran <keircn@proton.me>
2025-12-03 02:48:36 +0000

    linter

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, AnimatePresence } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
