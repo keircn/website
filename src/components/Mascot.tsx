3fb12c148d8ec8de139119f5b4df77e03609ae4c
Keiran <keircn@proton.me>
2025-12-17 00:05:30 +0000

    use transparent roxy gif

--- src/components/Mascot.tsx
+++ src/components/Mascot.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Mascot() {
   return (
     <div className="fixed bottom-4 right-4 z-50 pointer-events-none">
-      <div className="rounded-lg overflow-hidden mix-blend-screen dark:mix-blend-normal dark:bg-black">
+      <div className="rounded-lg overflow-hidden bg-transparent">
         <Image
           src="/roxy.gif"
           alt="Roxy"
